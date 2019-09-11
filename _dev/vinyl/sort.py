ed4d54e450a562de97bb10e3ec62dbf1f9877a7e
Your Name <jadaytime@gmail.com>
2019-09-11 11:16:53 -0700

    update link to debut carbon turntable

--- _dev/vinyl/sort.py
+++ _dev/vinyl/sort.py
@@ -8,7 +8,7 @@ data:
     route: misc
     type: misc
 ---
-I've been collecting vinyl since I got my <a href="http://www.project-audio.com/main.php?prod=debutcarbon">Pro-Ject Debut Carbon</a> in November 2016. My collection has grown steadily since then. Here are the albums I have, in lexicographic order by artist.
+I've been collecting vinyl since I got my <a href="https://www.project-audio.com/en/product/debut-carbon-dc/">Pro-Ject Debut Carbon</a> in November 2016. My collection has grown steadily since then. Here are the albums I have, in lexicographic order by artist.
 
 | **Artist** | **Album** |
 | :-- | --- |"""
